b1cf36d155a546428fbc955d05c32557fe65d440
yassineamri722 <amriyassine722@gmail.com>
2025-10-13 10:00:58 -0400

    ref:refactor dashboard to display more details about jetson minotoring with the updated script jetson_fetch.sh in jetson

--- scripts/fix-permissions.sh
+++ scripts/fix-permissions.sh
@@ -6,14 +6,14 @@ echo "[Init] Fixing host folder permissions..."
 if [ -d ./grafana/data ]; then
   echo "Fixing Grafana data, dashboards, provisioning..."
   sudo chown -R 472:472 ./grafana/data ./grafana/dashboards ./grafana/provisioning
-  sudo chmod -R 755 ./grafana/data ./grafana/dashboards ./grafana/provisioning
+  sudo chmod -R 777 ./grafana/data ./grafana/dashboards ./grafana/provisioning
 fi
 
 # Prometheus
 if [ -d ./prometheus/data ]; then
   echo "Fixing Prometheus data..."
   sudo chown -R 65534:65534 ./prometheus/data
-  sudo chmod -R 755 ./prometheus/data
+  sudo chmod -R 777 ./prometheus/data
 fi
 
 echo "[Init] Permissions fixed successfully!"
